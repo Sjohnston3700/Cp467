aad230ae40ea6d0b3c531279750675f27a59ad59
Sarah Johnston <sjohnston3700@gmail.com>
2018-10-04 10:00:34 -0400

    A2 Done

---
+++ assignments/eclipse_project/src/assignments/Main.java
@@ -0,0 +1,21 @@
+package assignments;
+
+public class Main {
+  public static void main(final String args[]) throws Exception {
+	  
+	///////////////////////////// A1 //////////////////////////
+	// Sharpen
+//	float[] operator = { 0, -1, 0, -1, 5f, -1, 0, -1, 0 };
+	
+	// Blur
+//	float[] operator = { 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f};
+//
+//    Image image = new Image("cat.jpg");
+//    image.apply3DFilter(operator, "image_altered.jpg");
+    ///////////////////////////// A1 //////////////////////////
+    
+    Image image = new Image("segments2.jpg");
+    //image.makeImageBlackAndWhite("image_altered.jpg");
+    image.findSegments();
+  }
+}
