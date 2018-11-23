4bababbe5109f53a1c4b81c9fc854e1aedc721b2
Sarah Johnston <sjohnston3700@gmail.com>
2018-11-22 20:00:21 -0500

    Did most of the report

--- assignments/eclipse_project/src/assignments/Main.java
+++ assignments/eclipse_project/src/assignments/Main.java
@@ -11,26 +11,16 @@ public class Main {
 		// float[] operator = { 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f};
 		// A1.run("cat.jpg", operator, "cat_a1.jpg");
 
-		///////////////////////////////////////////////////////////
+		//A2.run("segments2.jpg", "black_and_white.jpg");
 
-		///////////////////////////// A2 //////////////////////////
+		//A3.run("cat.jpg", "cat_a3_1.jpg", "cat_a3_2.jpg");
 
-		// A2.run("segments2.jpg");
-
-		////////////////////////////////////////////////////////////
-
-		///////////////////////////// A3 //////////////////////////
-
-		// A3.run("cat.jpg", "cat_a3_1.jpg", "cat_a3_2.jpg");
-
-		////////////////////////////////////////////////////////////
-
-		// A4.run("segments2.jpg", 0.5f, "segments0_75.jpg");
+		A4.run("segments2.jpg", 0.75f, "segments2_scaled.jpg");
 		
 		// A5.run("digits.jpg");
 		
 		// A6.run("digits.jpg", "digit_zoning_FVs.csv");
 		
-		A8.run("segments2.jpg", "segments2_thinned.jpg");
+		//A8.run("segments2.jpg", "segments2_thinned.jpg");
 	}
 }
